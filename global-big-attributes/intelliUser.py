6e2e85b5f3a6fea8a52f76ada1a2882428325b32
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-05-12 03:08:36 +0200

    changes in intelliUser.py

--- global-big-attributes/intelliUser.py
+++ global-big-attributes/intelliUser.py
@@ -22,3 +22,27 @@ class GitUser():
 
     def getLogin(self):
         return self.login
+
+    def setFullname(self, fullname):
+        self.fullname = fullname
+
+    def getFullname(self):
+        return self.fullname
+
+    def setFollowers(self, followers):
+        self.followers = followers
+
+    def getFollowers(self):
+        return self.followers
+
+    def setFollowing(self, following):
+        self.following = following
+
+    def getFollowing(self):
+        return self.following
+
+    def setRepositories(self, repositories):
+        self.repositories = repositories
+
+    def getRepositories(self):
+        return self.repositories
