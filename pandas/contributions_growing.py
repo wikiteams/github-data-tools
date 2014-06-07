2aec228199e176ca99e1e7f2312bdc1794ecef09
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-06-07 05:21:48 +0200

    updating contributions_growing.py, making progress

--- pandas/contributions_growing.py
+++ pandas/contributions_growing.py
@@ -1,4 +1,5 @@
 import pandas
+import dateutil.parser
 
 created_at_filename = 'created.csv'
 
@@ -6,16 +7,24 @@ pushes_csv_filename = 'pushes.csv'
 pulls_csv_filename = 'pulls.csv'
 follows_csv_filename = 'follows.csv'
 issues_csv_filename = 'issues.csv'
-team_adds_csv_filename = 'team_adds.csv'
+team_adds_csv_filename = 'team-adds.csv'
 
 ultimate_path = '../'
 
+#dtypes = [str, str, str, str, str, str, str, str]
 team_adds_df = pandas.read_csv(ultimate_path + team_adds_csv_filename, header=0,
-                             sep=',', na_values=['', None], error_bad_lines=False, parse_dates=[1], quotechar='"')
+                               sep=',', na_values=['', None], error_bad_lines=False, quotechar='"')
 print 'Reading team_adds_csv_filename done.'
 print team_adds_df.dtypes
 print team_adds_df.head()
 print team_adds_df.tail()
+print 'Parsing IsoDate Zulu time to proper datetime object'
+team_adds_df['created_at'] = team_adds_df['created_at'].apply(lambda x: dateutil.parser.parse(x))
+print team_adds_df.dtypes  # can verify
+print team_adds_df.head()
+print team_adds_df.tail()
+
+raw_input("Press Enter to continue...")
 
 created_df = pandas.read_csv(ultimate_path + created_at_filename, header=0,
                              sep=',', na_values=['', None], error_bad_lines=False, parse_dates=[5], quotechar='"')
